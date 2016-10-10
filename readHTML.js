4f38cc87e55a00dc61e4828eb99c566ae3b19570
April Mulvey <april.mulvey@gmail.com>
2016-10-10 18:40:16 +0000

    commiting working solutions to readHTML and with request and npm request module

---
+++ readHTML.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+function requestOptions(url, callback) {
+  var requestOptions = {
+    host: "example.com",
+    path: "/"
+  };
+
+  http.get(requestOptions, function printHTML(htmlData) {    // HTTP Response Callback
+
+    htmlData.setEncoding("utf8");             // Use UTF-8 encoding
+
+    htmlData.on("data", function (data) {           // On Data Received
+      console.log("Chunk Received. Data:",data);
+    });
+
+    htmlData.on("end", function () {                // On Data Completed
+      console.log("Response stream complete.");
+    });
+
+  });
+}
+requestOptions();
